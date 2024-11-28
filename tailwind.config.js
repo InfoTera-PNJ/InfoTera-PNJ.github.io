eb34a02b2ee4e6efc8bac80f6ae106f9c62d797d
lvciddreams <farrelmaahira104@gmail.com>
2024-11-28 23:22:44 +0700

    making blog page and single blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/flowbite/**/*.{js}",
+    "./node_modules/flowbite/**/*.js",
     "index.html",
     "./pages/*.{html,js}"
   ],
