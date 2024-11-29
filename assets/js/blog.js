3e6a0fbd91eaf032d31639588f1a01b7e943f17f
lvciddreams <farrelmaahira104@gmail.com>
2024-11-30 00:40:02 +0700

    finishin single blog, add carousel

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -15,16 +15,17 @@ const renderData = async () => {
 
     data.map(item => {
         cards += `<a href="single-blog.html?blog=${item.slug}" class="flex flex-col animate-fadeindown items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-3">
-                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="./../assets/images/application.jpg" alt="">
+                <div class="w-full md:w-48 h-96 md:h-auto overflow-hidden rounded-t-l md:rounded-none md:rounded-s-lg"> 
+                    <img class="w-full h-full object-cover" src="${item.image}" alt="">
+                </div>
                 <div class="flex flex-col justify-between leading-normal">
                     <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">${item.judul}</h5>
                 </div>
             </a>`
+
+            console.log(item)
     });
-
     contentSection.innerHTML = cards;
-
-    console.log(data);
 }
 
 renderData();
